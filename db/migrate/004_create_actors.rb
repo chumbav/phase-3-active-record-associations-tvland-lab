6bfcd8740877cf8b701927725bbdb672c2bdbe31
Victor <victor.chumba@student.moringaschool.com>
2023-06-09 22:58:09 +0300

    completed

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,5 +1,9 @@
 class CreateActors < ActiveRecord::Migration[6.1]
   def change
     # Your code here
+    create_table :actors do |t|
+      t.strings :first_name
+      t.strings :tast_name
+    end
   end
 end
